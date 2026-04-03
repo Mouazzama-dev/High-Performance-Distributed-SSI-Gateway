612e07d7fb46b0ab4de8082795613dbb059b7555
Mouazzama-dev <mouazzamabinteumer.ep19102044@gmail.com>
2026-04-03 02:56:58 +0200

    changed did:ether to did :key

--- agent.js
+++ agent.js
@@ -2,31 +2,36 @@ import 'reflect-metadata'
 
 import { createAgent } from '@veramo/core'
 import { DIDManager } from '@veramo/did-manager'
-import { EthrDIDProvider } from '@veramo/did-provider-ethr'
+import { KeyDIDProvider } from '@veramo/did-provider-key'
 import { KeyManager } from '@veramo/key-manager'
 import { KeyManagementSystem, SecretBox } from '@veramo/kms-local'
 import { CredentialPlugin } from '@veramo/credential-w3c'
 import { CredentialProviderJWT } from '@veramo/credential-jwt'
 import { DIDResolverPlugin } from '@veramo/did-resolver'
-import { getResolver as ethrDidResolver } from 'ethr-did-resolver'
-import { getResolver as webDidResolver } from 'web-did-resolver'
-import { Entities, KeyStore, DIDStore, PrivateKeyStore, migrations } from '@veramo/data-store'
+import { getResolver as keyDidResolver } from 'key-did-resolver'
+
+import {
+  Entities,
+  KeyStore,
+  DIDStore,
+  PrivateKeyStore,
+  migrations,
+} from '@veramo/data-store'
+
 import { DataSource } from 'typeorm'
 import { DataStore, DataStoreORM } from '@veramo/data-store'
-
 
 /***************************************
  * CONFIGURATION
  ***************************************/
-
 const DATABASE_FILE = 'database.sqlite'
-const INFURA_PROJECT_ID = ''   // ← leave empty for now
-const KMS_SECRET_KEY = 'de1e045d46d2a99bff4fd8a7c3f11ca4e3c65e57603e15c499a26c5b1ea5929b'
+
+const KMS_SECRET_KEY =
+  'de1e045d46d2a99bff4fd8a7c3f11ca4e3c65e57603e15c499a26c5b1ea5929b'
 
 /***************************************
  * DATABASE INITIALIZATION
  ***************************************/
-
 const dbConnection = await new DataSource({
   type: 'sqlite',
   database: DATABASE_FILE,
@@ -40,10 +45,8 @@ const dbConnection = await new DataSource({
 /***************************************
  * VERAMO AGENT
  ***************************************/
-
 export const agent = createAgent({
   plugins: [
-
     new KeyManager({
       store: new KeyStore(dbConnection),
       kms: {
@@ -56,37 +59,24 @@ export const agent = createAgent({
       },
     }),
 
+    // ✅ DID:key instead of did:ethr
     new DIDManager({
       store: new DIDStore(dbConnection),
-      defaultProvider: 'did:ethr:sepolia',
+      defaultProvider: 'did:key',
       providers: {
-        'did:ethr:sepolia': new EthrDIDProvider({
+        'did:key': new KeyDIDProvider({
           defaultKms: 'local',
-          network: 'sepolia',
-          rpcUrl: 'https://ethereum-sepolia-rpc.publicnode.com',
         }),
       },
     }),
 
-        new DataStore(dbConnection),
+    new DataStore(dbConnection),
     new DataStoreORM(dbConnection),
 
-
-new DIDResolverPlugin({
-  ...ethrDidResolver({
-    networks: [
-      {
-        name: 'sepolia',
-        chainId: 11155111,
-        rpcUrl: 'https://ethereum-sepolia-rpc.publicnode.com',
-        registry: '0x03d5003bf0e79C5F5223588F347ebA39AfbC3818',
-      },
-    ],
-  }),
-  ...webDidResolver(),
-}),
-
-
+    // ✅ Resolver for did:key
+    new DIDResolverPlugin({
+      ...keyDidResolver(),
+    }),
 
     new CredentialPlugin({
       credentialProviders: [new CredentialProviderJWT()],
